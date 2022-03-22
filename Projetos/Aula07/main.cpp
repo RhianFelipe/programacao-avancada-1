b094bec3029baeb205f906b67734d61d84dc5a50
Chaua Queirolo <chaua@users.noreply.github.com>
2022-03-21 22:23:34 -0300

    :sparkles: Implementação da herança

--- Projetos/Aula07/main.cpp
+++ Projetos/Aula07/main.cpp
@@ -1,12 +1,20 @@
-#include <iostream>
 
 #include "Triangulo.h"
+#include "Quadrado.h"
 
 int main() {
-    Triangulo t1(3, 4);
-    t1.desenha();       // com sobrescrita
-    t1.imprimeLados();  // sem sobrescrita
+    {
+        Triangulo t1(3, 4);
+        t1.desenha();       // com sobrescrita
+        t1.imprimeLados();  // sem sobrescrita
+    }
 
+    {
+        Quadrado q1(10);
+        q1.desenha();
+        q1.imprimeLados();
+        std::cout << "Area = " << q1.getArea() << std::endl;
+    }
 
     return 0;
 }
